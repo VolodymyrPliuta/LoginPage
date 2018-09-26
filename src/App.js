2592399cdab3734433f4a23c5978b426b4845ceb
americanman <snowboardracer7@gmail.com>
2018-09-26 16:38:01 -0400

    login div

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <div className="nav-menu">
-        <nav>
+        <nav className="nav">
           <ol>
             <li><a href="…">Home</a></li>
             <li><a href="…">Page2</a></li>
             <li><a href="…">Page3</a></li>
             <li><a href="…">Page4</a></li>
           </ol>
-          <button>Log In</button>
         </nav>
+          <div className="login">
+            <button>Log In</button>
+          <div>
       </div>
     );
   }
