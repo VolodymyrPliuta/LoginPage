ac13aef99fcac87c4b92c928b69c72a0fbe9a9a7
americanman <snowboardracer7@gmail.com>
2018-09-26 16:06:25 -0400

    Init commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="nav-menu">
+        <nav>
+          <ol>
+            <li><a href="…">Home</a></li>
+            <li><a href="…">Page2</a></li>
+            <li><a href="…">Page3</a></li>
+            <li><a href="…">Page4</a></li>
+          </ol>
+          <button>Log In</button>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default App;
